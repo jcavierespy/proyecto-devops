a1b9e078668754e2ca270c0121aab801612e7b07
root <jcavieres@py.cl>
2024-05-11 20:12:39 -0400

    prueba4

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import app from "./server.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("server started");
+  console.log("server started on port:", port);
 });
